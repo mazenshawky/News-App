ffa2f2b860d4eebab1cd4dc3d1e3f7998c284072
Mazen Shawky <mazenshawqy@gmail.com>
2023-01-13 02:11:41 +0200

    Fix webview

--- lib/modules/web_view/web_view_screen.dart
+++ lib/modules/web_view/web_view_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 // import 'package:webview_flutter/webview_flutter.dart';
 
-class WebViewScreen extends StatelessWidget
-{
+class WebViewScreen extends StatelessWidget {
   late final String url;
 
   WebViewScreen(this.url);
@@ -11,9 +11,9 @@ class WebViewScreen extends StatelessWidget
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      // body: WebView(
-      //   initialUrl: url,
-      // ),
+      body: WebView(
+        initialUrl: url,
+      ),
     );
   }
 }
--- lib/shared/components/components.dart
+++ lib/shared/components/components.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 
+import '../../modules/web_view/web_view_screen.dart';
+
 Widget defaultButton({
   double width = double.infinity,
   Color background = Colors.blue,
   bool isUpperCase = true,
   double radius = 3.0,
-
   required void Function() function,
 //required VoidCallback function,
   required String text,
@@ -52,252 +53,251 @@ Widget defaultFormField({
   IconData? suffix,
   VoidCallback? suffixPressed,
   bool isClickable = true,
-}) => TextFormField(
-  controller: controller,
-  keyboardType: type,
-  obscureText: isPassword,
-  enabled: isClickable,
-  onFieldSubmitted: onSubmit,
-  onChanged: onChange,
-  onTap: onTap,
-  validator: validate,
-  decoration: InputDecoration(
-    labelText: label,
-    prefixIcon: Icon(
-      prefix,
-    ),
-    suffixIcon: suffix != null ? IconButton(
-      onPressed: suffixPressed,
-      icon: Icon(
-        suffix,
+}) =>
+    TextFormField(
+      controller: controller,
+      keyboardType: type,
+      obscureText: isPassword,
+      enabled: isClickable,
+      onFieldSubmitted: onSubmit,
+      onChanged: onChange,
+      onTap: onTap,
+      validator: validate,
+      decoration: InputDecoration(
+        labelText: label,
+        prefixIcon: Icon(
+          prefix,
+        ),
+        suffixIcon: suffix != null
+            ? IconButton(
+                onPressed: suffixPressed,
+                icon: Icon(
+                  suffix,
+                ),
+              )
+            : null,
+        border: OutlineInputBorder(),
       ),
-    ) : null,
-    border: OutlineInputBorder(),
-  ),
-);
+    );
 
 PreferredSizeWidget defaultAppBar({
   required BuildContext context,
   String? title,
   List<Widget>? actions,
-}) => AppBar(
-  leading: IconButton(
-    onPressed: ()
-    {
-      Navigator.pop(context);
-    },
-    icon: Icon(
-      Icons.arrow_back_ios,
-    ),
-  ),
-  titleSpacing: 0.0,
-  title: Text(
-    title!,
-  ),
-  actions: actions,
-);
+}) =>
+    AppBar(
+      leading: IconButton(
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        icon: Icon(
+          Icons.arrow_back_ios,
+        ),
+      ),
+      titleSpacing: 0.0,
+      title: Text(
+        title!,
+      ),
+      actions: actions,
+    );
 
 Widget buildTaskItem(Map model, context) => Dismissible(
-  key: Key(model['id'].toString()),
-  child: Padding(
-    padding: EdgeInsets.all(20.0),
-    child: Row(
-      children:
-      [
-        CircleAvatar(
-          radius: 40.0,
-          child: Text(
-            '${model['time']}',
-          ),
-        ),
-        SizedBox(
-          width: 20.0,
-        ),
-        Expanded(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children:
-            [
-              Text(
-                '${model['title']}',
-                style: TextStyle(
-                  fontSize: 18.0,
-                  fontWeight: FontWeight.bold,
-                ),
+      key: Key(model['id'].toString()),
+      child: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Row(
+          children: [
+            CircleAvatar(
+              radius: 40.0,
+              child: Text(
+                '${model['time']}',
               ),
-              Text(
-                '${model['date']}',
-                style: TextStyle(
-                  color: Colors.grey,
-                ),
-              ),
-            ],
-          ),
-        ),
-        SizedBox(
-          width: 20.0,
-        ),
-        IconButton(
-            onPressed: ()
-            {
-              // AppCubit.get(context).updateData(
-              //     status: 'done',
-              //     id: model['id'],
-              // );
-            },
-            icon: Icon(
-              Icons.check_box,
-              color: Colors.green,
             ),
+            SizedBox(
+              width: 20.0,
+            ),
+            Expanded(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    '${model['title']}',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    '${model['date']}',
+                    style: TextStyle(
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              width: 20.0,
+            ),
+            IconButton(
+              onPressed: () {
+                // AppCubit.get(context).updateData(
+                //     status: 'done',
+                //     id: model['id'],
+                // );
+              },
+              icon: Icon(
+                Icons.check_box,
+                color: Colors.green,
+              ),
+            ),
+            IconButton(
+              onPressed: () {
+                // AppCubit.get(context).updateData(
+                //   status: 'archive',
+                //   id: model['id'],
+                // );
+              },
+              icon: Icon(
+                Icons.archive,
+                color: Colors.black45,
+              ),
+            ),
+          ],
         ),
-        IconButton(
-          onPressed: ()
-          {
-            // AppCubit.get(context).updateData(
-            //   status: 'archive',
-            //   id: model['id'],
-            // );
-          },
-          icon: Icon(
-            Icons.archive,
-            color: Colors.black45,
-          ),
-        ),
-      ],
-    ),
-  ),
-  onDismissed: (direction)
-  {
-    // AppCubit.get(context).deleteData(id: model['id'],);
-  },
-);
+      ),
+      onDismissed: (direction) {
+        // AppCubit.get(context).deleteData(id: model['id'],);
+      },
+    );
 
 Widget tasksBuilder({
   required List<Map> tasks,
-}) => tasks.isNotEmpty ?
-ListView.separated(
-  itemBuilder: (context, index) =>
-      buildTaskItem(tasks[index], context),
-  separatorBuilder: (context, index) =>
-      myDivider(),
-  itemCount: tasks.length,
-)
-    : Center(
-  child: Column(
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: [
-      Icon(
-        Icons.menu,
-        size: 100.0,
-        color: Colors.grey,
-      ),
-      Text(
-        'No Tasks Yet, Please Add Some Tasks',
-        style: TextStyle(
-          fontSize: 16.0,
-          fontWeight: FontWeight.bold,
-          color: Colors.grey,
-        ),
-      ),
-    ],
-  ),
-);
-
-Widget myDivider() => Padding(
-  padding: EdgeInsetsDirectional.only(
-    start: 20.0,
-  ),
-  child: Container(
-    width: double.infinity,
-    height: 1.0,
-    color: Colors.grey[300],
-  ),
-);
-
-Widget buildArticleItem(article, context) => InkWell(
-  onTap: (){
-    // navigateTo(context, WebViewScreen(article['url']),);
-  },
-  child: Padding(
-    padding: EdgeInsets.all(20.0),
-    child: Row(
-      children: [
-        Container(
-          width: 120.0,
-          height: 120.0,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10.0,),
-            image: DecorationImage(
-              image: NetworkImage('${article['urlToImage']}'),
-              fit: BoxFit.cover,
-            ),
-          ),
-        ),
-        SizedBox(
-          width: 20.0,
-        ),
-        Expanded(
-          child: Container(
-            height: 120.0,
+}) =>
+    tasks.isNotEmpty
+        ? ListView.separated(
+            itemBuilder: (context, index) =>
+                buildTaskItem(tasks[index], context),
+            separatorBuilder: (context, index) => myDivider(),
+            itemCount: tasks.length,
+          )
+        : Center(
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Expanded(
-                  child: Text(
-                    '${article['title']}',
-                    style: Theme.of(context).textTheme.bodyText1,
-                    maxLines: 3,
-                    overflow: TextOverflow.ellipsis,
-                  ),
+                Icon(
+                  Icons.menu,
+                  size: 100.0,
+                  color: Colors.grey,
                 ),
                 Text(
-                  '${article['publishedAt']}',
+                  'No Tasks Yet, Please Add Some Tasks',
                   style: TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold,
                     color: Colors.grey,
                   ),
                 ),
               ],
             ),
-          ),
-        ),
-      ],
-    ),
-  ),
-);
+          );
 
-Widget articleBuilder(list, context, {isSearch = false}) => list.length > 0 ?
-ListView.separated(
-  physics: BouncingScrollPhysics(),
-  itemBuilder: (context, index) => buildArticleItem(list[index], context),
-  separatorBuilder: (context, index) => myDivider(),
-  itemCount: list.length,
-)
-    : isSearch ?
-Container()
-    : Center(child: CircularProgressIndicator());
+Widget myDivider() => Padding(
+      padding: EdgeInsetsDirectional.only(
+        start: 20.0,
+      ),
+      child: Container(
+        width: double.infinity,
+        height: 1.0,
+        color: Colors.grey[300],
+      ),
+    );
+
+Widget buildArticleItem(article, context) => InkWell(
+      onTap: () {
+        navigateTo(
+          context,
+          WebViewScreen(article['url']),
+        );
+      },
+      child: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Row(
+          children: [
+            Container(
+              width: 120.0,
+              height: 120.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(
+                  10.0,
+                ),
+                image: DecorationImage(
+                  image: NetworkImage('${article['urlToImage']}'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 20.0,
+            ),
+            Expanded(
+              child: Container(
+                height: 120.0,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      child: Text(
+                        '${article['title']}',
+                        style: Theme.of(context).textTheme.bodyText1,
+                        maxLines: 3,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                    Text(
+                      '${article['publishedAt']}',
+                      style: TextStyle(
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+
+Widget articleBuilder(list, context, {isSearch = false}) => list.length > 0
+    ? ListView.separated(
+        physics: BouncingScrollPhysics(),
+        itemBuilder: (context, index) => buildArticleItem(list[index], context),
+        separatorBuilder: (context, index) => myDivider(),
+        itemCount: list.length,
+      )
+    : isSearch
+        ? Container()
+        : Center(child: CircularProgressIndicator());
 
 void navigateTo(context, widget) => Navigator.push(
-  context,
-  MaterialPageRoute(
-    builder: (context) => widget,
-  ),
-);
-
-void navigateAndFinish(
-    context, widget
-    ) =>
-    Navigator.pushAndRemoveUntil(
       context,
       MaterialPageRoute(
         builder: (context) => widget,
       ),
-        (route) => false,
-);
+    );
 
-void printFullText(String text)
-{
+void navigateAndFinish(context, widget) => Navigator.pushAndRemoveUntil(
+      context,
+      MaterialPageRoute(
+        builder: (context) => widget,
+      ),
+      (route) => false,
+    );
+
+void printFullText(String text) {
   final pattern = RegExp('.{1,800}');
   pattern.allMatches(text).forEach((match) => print(match.group(0)));
 }
